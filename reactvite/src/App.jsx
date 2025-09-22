b030648db0ed77167c47c46d5b75d6f1cffc84cb
SOMSOC-RKD <das794176@gmail.com>
2025-09-22 10:24:37 +0530

    first commit

---
+++ reactvite/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import Rkd from './rkd'
+
+function App() {
+  
+
+  return (
+    <>
+  <Rkd/>
+  <h1>hellow worldcd</h1>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore iure sapiente blanditiis reiciendis totam velit ratione eveniet id quo assumenda. Ratione mollitia dolores aliquid eos, accusamus praesentium suscipit voluptate ea.</p>
+     </>
+   
+  )
+}
+
+export default App
---
+++ reactvite/src/rkd.jsx
@@ -0,0 +1,9 @@
+function Rkd (){
+
+    return(
+
+        <h3>ready for project</h3>
+    )
+}
+
+export default Rkd
