d60cfbc93e5d3080703c020efef811a71bd6cf71
SOMSOC-RKD <das794176@gmail.com>
2025-09-22 10:26:56 +0530

    change

--- reactvite/src/App.jsx
+++ reactvite/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
   <Rkd/>
   <h1>hellow worldcd</h1>
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore iure sapiente blanditiis reiciendis totam velit ratione eveniet id quo assumenda. Ratione mollitia dolores aliquid eos, accusamus praesentium suscipit voluptate ea.</p>
+  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore iure sapiente blanditiis reiciendis totam velit ratione eveniet id quo assumenda. Ratione mollitia dolores aliquid eos, accusamus praesentium.</p>
      </>
    
   )
